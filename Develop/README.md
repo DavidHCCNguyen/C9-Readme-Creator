61aabdefe06c0ac671102cec1f25e6155fedd8a9
DavidHCCNguyen <creativespecials@gmail.com>
2023-05-18 23:29:51 +0930

    Video, explaining

---
+++ Develop/README.md
@@ -0,0 +1,42 @@
+# A readme generator
+
+![License](https://img.shields.io/badge/License-ISC-blue.svg)
+
+## Description
+
+This is a code that runs the code from index.js, which will create a readme file by asking questions.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+you run the code by right clicking develop in vscode, click it's terminal, and then you write "node index.js"
+
+## Usage
+
+this project will create a readme without having to type everything letter by letter, instead the user will ask questions and the code will do the rest for them.
+
+## License
+
+This application is covered under the ISC license.
+
+## Contributing
+
+by either pulling this repo, creating a different branch, and asking for a review, either it goes through or it doesn't
+
+## Tests
+
+repeat if this works.
+
+## Questions
+
+GitHub: [DavidHCCNguyen](https://github.com/DavidHCCNguyen)
+
+For any additional questions, feel free to reach out to me via email: [creativespecials@gmail.com](mailto:creativespecials@gmail.com)
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,115 @@
 // TODO: Include packages needed for this application
+const readmeDocs = require('readme-docs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? ',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project: ',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions? ',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project? ',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'ISC', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide guidelines for contributing to the project: ',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide instructions on how to run tests: ',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username: ',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address: ',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const {
+            title,
+            description,
+            installation,
+            usage,
+            license,
+            contributing,
+            tests,
+            github,
+            email,
+        } = answers;
+
+        // Generate license badge
+        const licenseBadge = `![License](https://img.shields.io/badge/License-${encodeURIComponent(
+            license
+        )}-blue.svg)`;
+
+        // Generate license notice
+        const licenseNotice = `This application is covered under the ${license} license.`;
+
+        // Generate GitHub profile link
+        const githubLink = `GitHub: [${github}](https://github.com/${github})`;
+
+        // Generate email contact
+        const emailContact = `For any additional questions, feel free to reach out to me via email: [${email}](mailto:${email})`;
+
+        const data = `# ${title}\n\n${licenseBadge}\n\n## Description\n\n${description}\n\n## Table of Contents\n\n- [Installation](#installation)\n- [Usage](#usage)\n- [License](#license)\n- [Contributing](#contributing)\n- [Tests](#tests)\n- [Questions](#questions)\n\n## Installation\n\n${installation}\n\n## Usage\n\n${usage}\n\n## License\n\n${licenseNotice}\n\n## Contributing\n\n${contributing}\n\n## Tests\n\n${tests}\n\n## Questions\n\n${githubLink}\n\n${emailContact}`;
+
+        writeToFile('README.md', data);
+
+        // Configuration options for generateDocs function
+        const options = {
+            inputFile: '../README.md',
+            outputFile: '../output.html',
+        };
+
+        // Generate documentation using readme-docs
+        readmeDocs
+            .generateDocs(options)
+            .then(() => {
+                console.log('Documentation generated successfully!');
+            })
+            .catch((error) => {
+                console.error('An error occurred while generating documentation:', error);
+            });
+    });
+}
 
 // Function call to initialize app
 init();
+
+// node app.js
